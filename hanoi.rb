01a2283c447c84642a1f6da9911a968be1e6f130
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-12-14 00:10:24 -0500

    finished new display method/win condition generator - again at figuring out user movement

--- hanoi.rb
+++ hanoi.rb
@@ -1,27 +1,54 @@
 
-def render()
+# Documents/Viking/Ruby/Hanoi
+
+=begin
+  Rules:
+    1. Only one disk can be moved at a time.
+    2. Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack.
+    3. No disk may be placed on top of a smaller disk.
+=end
+
+# need to display the rules/directions somewhere
+
+def winCondition(height, solution)
+  disc = 1
+  while disc <= height
+    size = ("o" * disc)
+    solution << size
+    disc += 1
+  end
+  return solution
+end
+
+def render(a, b, c)
+  system "cls"
+  system "clear"
+  puts a
+  puts "\n-A-"
+  puts b
+  puts "\n-B-"
+  puts c
+  puts "\n-C-"
 end
 
 def move()
 end
 
-input = 0
-height = 0
+a, b, c, solution = [], [], [], []
+input, height = 0, 0
 print "How many discs would you like there to be in the tower? "
 height = gets.chomp.to_i
 # need to handle bad inputs
-
-# create a method to generate win condition based on tower height
+winCondition(height, solution)
+a = solution
 
 while input != "quit"
-
-# need to adjust to match new approach
-  if board == winCondition
+  if c == solution
     puts "you won!"
     exit
   end
 
-render()
+render(a, b, c)
 
 # create a method to process moving discs, it needs to handle moves that are against the rules
 move()
@@ -29,3 +56,12 @@ move()
   input = gets.chomp
 # need to handle bad inputs
 end
+
+
+
+
+
+
+
+
+# spacing
