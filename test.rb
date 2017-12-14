2aee2fc3c87eb11c24c929a406455e836ec65aab
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-12-14 17:42:23 -0500

    saving progress, evaluating different ways of handling illegal moves

--- test.rb
+++ test.rb
@@ -1,34 +1,55 @@
+height = 3
 
 a = [1, 2, 3]
 b = [0]
-limit = 1
 
 print "\nEnter where you'd like to move from "
 from = gets.chomp
-
 print "\nEnter where you'd like to move to "
 to = gets.chomp
 
-while limit == 1
-  if from == "a" && to == "b"
-    if b == []
-      b << a[0]
-      a.shift
-      puts "breaking"
-      break
-    end
-    if a[0] < b[0]
-      b << a[0]
-      a.shift
-      puts "moved"
-    end
-      puts "eh"
-  else
-    puts "nope"
+if b == []
+  b = [height + 1]
+end
+
+if from == "a" && to == "b"
+  if a[0] > b[0]
+    # split this part out into a seperate method to avoid repetition ?
+    # or add to the if logic check and make the else statement a longer reminder of what's allowed?
+    puts "illegal move"
+    exit # break
   end
-    limit -= 1
+  b << a[0]
+  a.shift
+elsif from == "a" && to == "c"
+  c << a[0]
+  a.shift
+elsif from == "b" && to == "c"
+  c << b[0]
+  b.shift
+elsif from == "b" && to == "a"
+  a << b[0]
+  b.shift
+elsif from == "c" && to == "a"
+  a << c[0]
+  c.shift
+elsif from == "c" && to == "b"
+  b << c[0]
+  c.shift
+elsif to == "quit"
+  exit
+else
+  puts "\nPlease either enter quit to end the game or one of the following letters\na\nb\nc\n"
+end
+
+if b.include?(height + 1)
+  b.delete_at(b.index(height + 1))
 end
 
 puts a
-puts "newline"
+puts "newline end show"
 puts b
+
+# first an if block to make empty arrays equal to max + 1
+
+# second if block has existing logic plus blocking bigger disc on smaller disc
