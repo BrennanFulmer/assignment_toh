9cb3cabe334c3080ba377307e15427cc79ae1e44
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-12-13 21:26:56 -0500

    adjusting for approach as old method would make moving require too many calculations

--- hanoi.rb
+++ hanoi.rb
@@ -1,80 +1,31 @@
 
-# Documents/Viking/Ruby/Hanoi
-
-=begin
-  Rules:
-    1. Only one disk can be moved at a time.
-    2. Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack.
-    3. No disk may be placed on top of a smaller disk.
-=end
-
-def initialBoard(height, spacing, board)
-  disc = 0
-  while disc < height
-    disc += 1
-    size = ("o" * disc) + " " * (height - disc)
-    board["disc#{disc}"] = size + spacing * 2
-  end
-  board["base"] = "1" + spacing + "2" + spacing + "3"
-  return board
+def render()
 end
 
-def winCondition(height, spacing, winCondition)
-  disc = 0
-  while disc < height
-    disc += 1
-    size = ("o" * disc) + " " * (height - disc)
-    winCondition["disc#{disc}"] = spacing * 2 + size
-  end
-  winCondition["base"] = "1" + spacing + "2" + spacing + "3"
-  return winCondition
+def move()
 end
 
-# make this create a prettier display later
-def render(height, board)
-  disc = 0
-  while disc < height
-    disc += 1
-    puts board["disc#{disc}"]
-  end
-  puts board["base"]
-end
-
-# this will become initialize in the class version
 input = 0
-board = {}
-inputy = 0
-winCondition = {}
+height = 0
 print "How many discs would you like there to be in the tower? "
-inputy = gets.chomp
-height = inputy.to_i
-spacing = " " * height
-initialBoard(height, spacing, board)
-winCondition(height, spacing, winCondition)
+height = gets.chomp.to_i
+# need to handle bad inputs
+
+# create a method to generate win condition based on tower height
 
 while input != "quit"
 
+# need to adjust to match new approach
   if board == winCondition
     puts "you won!"
     exit
   end
 
-  render(height, board)
-
-  
+render()
 
 # create a method to process moving discs, it needs to handle moves that are against the rules
+move()
 
   input = gets.chomp
+# need to handle bad inputs
 end
-
-
-
-
-
-
-
-
-
-
-# spacing
