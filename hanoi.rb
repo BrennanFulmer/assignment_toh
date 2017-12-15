14f448ed7630f6b7cea7b708172cc0b398545951
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-12-14 23:29:17 -0500

    minor indentation changes, just need to make some aesthetic changes to finish the project

--- hanoi.rb
+++ hanoi.rb
@@ -58,35 +58,35 @@ class Hanoi
     end
     print "\nEnter where you'd like to move to "
     to = gets.chomp
-    @c = [@height + 1] if @c == []
-      @b = [@height + 1] if @b == []
-        @a = [@height + 1] if @a == []
-          if from == "a" && to == "b" && @a[0] < @b[0]
-            @b.unshift(@a[0])
-            @a.shift
-          elsif from == "a" && to == "c" && @a[0] < @c[0]
-            @c.unshift(@a[0])
-            @a.shift
-          elsif from == "b" && to == "c" && @b[0] < @c[0]
-            @c.unshift(@b[0])
-            @b.shift
-          elsif from == "b" && to == "a" && @b[0] < @a[0]
-            @a.unshift(@b[0])
-            @b.shift
-          elsif from == "c" && to == "a" && @c[0] < @a[0]
-            @a.unshift(@c[0])
-            @c.shift
-          elsif from == "c" && to == "b" && @c[0] < @b[0]
-            @b.unshift(@c[0])
-            @c.shift
-          elsif to == "quit"
-            exit
-          else
-            puts "\nPlease either enter quit to end the game\nor one of the following letters when moving\na\nb\nc\nor remember that its against the rules to move a disk onto a smaller disk"
-          end
-            @c.delete_at(@c.index(@height + 1)) if @c.include?(@height + 1)
-              @b.delete_at(@b.index(@height + 1)) if @b.include?(@height + 1)
-                @a.delete_at(@a.index(@height + 1)) if @a.include?(@height + 1)
+      @c = [@height + 1] if @c == []
+        @b = [@height + 1] if @b == []
+          @a = [@height + 1] if @a == []
+            if from == "a" && to == "b" && @a[0] < @b[0]
+              @b.unshift(@a[0])
+              @a.shift
+            elsif from == "a" && to == "c" && @a[0] < @c[0]
+              @c.unshift(@a[0])
+              @a.shift
+            elsif from == "b" && to == "c" && @b[0] < @c[0]
+              @c.unshift(@b[0])
+              @b.shift
+            elsif from == "b" && to == "a" && @b[0] < @a[0]
+              @a.unshift(@b[0])
+              @b.shift
+            elsif from == "c" && to == "a" && @c[0] < @a[0]
+              @a.unshift(@c[0])
+              @c.shift
+            elsif from == "c" && to == "b" && @c[0] < @b[0]
+              @b.unshift(@c[0])
+              @c.shift
+            elsif to == "quit"
+              exit
+            else
+              puts "\nPlease either enter quit to end the game\nor one of the following letters when moving\na\nb\nc\nor remember that its against the rules to move a disk onto a smaller disk"
+            end
+              @c.delete_at(@c.index(@height + 1)) if @c.include?(@height + 1)
+                @b.delete_at(@b.index(@height + 1)) if @b.include?(@height + 1)
+                  @a.delete_at(@a.index(@height + 1)) if @a.include?(@height + 1)
   end
 
   def play
